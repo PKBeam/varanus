5069f4f89b133b7c6f541e602241237299508f31
Vincent Liu <pkbeam@vincents-macbook-pro.local>
2016-11-25 21:10:44 +1100

    Initial Commit

---
+++ Varanus/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Varanus
+//
+//  Created by Vincent Liu on 25/11/16.
+//  Copyright © 2016 Vincent Liu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Varanus/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Varanus
+//
+//  Created by Vincent Liu on 25/11/16.
+//  Copyright © 2016 Vincent Liu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
